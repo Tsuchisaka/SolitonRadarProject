fb37ab5e60c6ddc836a5b406d1da6b62119e1b5f
Tsuchisaka <tsucchii0824@gmail.com>
2014-09-19 21:44:06 +0900

    サーバと通信テスト
    サーバの通信テストとMACアドレスの取得まで

--- src/com/example/solitonradar/MainActivity.java
+++ src/com/example/solitonradar/MainActivity.java
@@ -2,23 +2,62 @@ package com.example.solitonradar;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
+import java.util.*;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.nifty.cloud.mb.*;
+import android.app.AlertDialog;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
+	PlayersPosition pp;
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //pp = new PlayersPosition();
+        //pp.setMyPosition(0.00, 0.00);
+        //setContentView(R.layout.activity_main);
+        
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.drawable.net);
         setContentView(imageView);
+        /*
+        NCMB.initialize(this, "480c1f99d7b45ae9459d50f303e95af736fe32392b914235b624c542d54ccf10", "29f491b4e283238a7ea6c18c1b369d9b39f8d507d7c3e30325fb48c4e14515e4");
+        
+        NCMBQuery<NCMBObject> query = NCMBQuery.getQuery("TestClass");
+        query.whereEqualTo("message", "Hello, Tarou!");
+        query.findInBackground(new FindCallback<NCMBObject>() {
+            @Override
+            public void done(List<NCMBObject> result, NCMBException e){
+                if (result.isEmpty() != true){
+                    dispMessage(result.get(0));
+                } else {
+                    PostData();
+                }
+            }
+        });*/
+        
     }
-
-
+    
+    private void PostData(){
+        NCMBObject TestClass = new NCMBObject("TestClass");
+        TestClass.put("message", "Hello, NCMB!");
+        TestClass.saveInBackground(); 
+    }
+    
+    private void dispMessage(NCMBObject message){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        
+        alertDialogBuilder.setTitle("ÉfÅ[É^éÊìæ");
+        alertDialogBuilder.setMessage(pp.getMacAddress());
+        alertDialogBuilder.show();
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ src/com/example/solitonradar/PlayersPosition.java
@@ -0,0 +1,54 @@
+package com.example.solitonradar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import java.util.*;
+
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.nifty.cloud.mb.*;
+
+import android.app.AlertDialog;
+
+public class PlayersPosition {
+	private String mac;
+	private boolean role_snake = false;
+	private int time = 0;
+	private boolean state = false;
+	
+	public PlayersPosition(){
+		mac = MacAddress.getMacAddressString();
+		
+	}
+	
+	public String getMacAddress(){return mac;}
+	
+	public void setMyPosition(final double X, final double Y){
+		//PlayerPositionクラスのMacAddressフィールドから自分のMacAddressを探す
+		NCMBQuery<NCMBObject> query = NCMBQuery.getQuery("PlayerPosition");
+		query.whereEqualTo("MacAddress", mac);
+		query.findInBackground(new FindCallback<NCMBObject>() {
+            @Override
+            public void done(List<NCMBObject> result, NCMBException e){
+            	String data = Double.toString(X) + "," + Double.toString(Y);
+                if (result.isEmpty() != true){
+                    //MacAddressが見つかった	
+                	NCMBObject obj = result.get(0);
+                	obj.put("Data", data);
+                	obj.saveEventually();
+                } else {
+                    //MacAddressが見つからなかった
+                	NCMBObject obj = new NCMBObject("TestClass");
+                	obj.put("MacAddress", mac);
+                    obj.put("Data", data);
+                    obj.saveInBackground(); 
+                }
+            }
+        });
+	}
+	
+	
+}
