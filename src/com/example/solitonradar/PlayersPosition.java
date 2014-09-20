cddd308f2bb468e877ed246e8e9000b1b44a2529
EriTakashima <tksmeri@gmail.com>
2014-09-20 20:51:10 +0900

    とりあえず統合したけど地図でたよ
    とりあえず地図は出るようになりましたがあいも変わらず現在位置がとれない(´・ω・`)
    あっ皆さんcloneした際は自分のAPIキーに書き換えてくださいねー！！

--- src/com/example/solitonradar/PlayersPosition.java
+++ src/com/example/solitonradar/PlayersPosition.java
@@ -15,10 +15,10 @@ import android.app.AlertDialog;
 import android.util.Log;
 
 public class PlayersPosition {
-	private PlayerData mydata;
-	public ArrayList<PlayerData> allPlayersData = new ArrayList<PlayerData>();
+	private PlayerData mydata;//自分のマックアドレスと座標と向きと役割の情報を保持（詳細はPlayerData参照）
+	public ArrayList<PlayerData> allPlayersData = new ArrayList<PlayerData>();//全員のデーターを保存するためのリスト
 	
-	public PlayersPosition(){
+	public PlayersPosition(){//初期化したい内容を書き込む
 		mydata = new PlayerData();
 		mydata.setMacAddress(MacAddress.getMacAddressString());
 	}
