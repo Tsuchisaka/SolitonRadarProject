5ee237e5067a5ced2c9245c33e073992ee6cfdbe
EriTakashima <tksmeri@gmail.com>
2014-09-23 22:51:10 +0900

    マップ下に文字表示出来たり，ボタン設置できるようになりました
    とりあえず設置しただけだけどね！

--- gen/com/example/solitonradar/R.java
+++ gen/com/example/solitonradar/R.java
@@ -1485,13 +1485,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f080028;
         public static final int action_menu_presenter=0x7f080029;
         public static final int action_mode_close_button=0x7f080036;
-        public static final int action_settings=0x7f080057;
+        public static final int action_settings=0x7f080059;
         public static final int activity_chooser_view_content=0x7f080037;
         public static final int always=0x7f08001e;
         public static final int beginning=0x7f080023;
         public static final int book_now=0x7f080010;
         public static final int btn1=0x7f080053;
-        public static final int btn2=0x7f080056;
+        public static final int btn2=0x7f080058;
+        public static final int button1=0x7f080057;
         public static final int buyButton=0x7f08000a;
         public static final int buy_now=0x7f08000f;
         public static final int buy_with_google=0x7f08000e;
@@ -1555,6 +1556,7 @@ containing a value of this type.
         public static final int strict_sandbox=0x7f080009;
         public static final int submit_area=0x7f08004b;
         public static final int tabMode=0x7f080015;
+        public static final int tap_text=0x7f080056;
         public static final int terrain=0x7f080003;
         public static final int title=0x7f08003d;
         public static final int top_action_bar=0x7f080032;
